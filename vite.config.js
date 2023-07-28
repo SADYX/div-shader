c073fec3957416b7042d9eecd462a33b31de878b
yangxiong <978345079@qq.com>
2023-07-28 15:34:58 +0800

    build setting

--- vite.config.js
+++ vite.config.js
@@ -1,7 +1,12 @@
 import { defineConfig } from 'vite';
-import { join } from 'path'
+import { join } from 'path';
 
 export default defineConfig({
     root: join(__dirname, 'src'),
     base: 'div-shader',
+    build: {
+        outDir: join(__dirname, "dist"),
+        emptyOutDir: true,
+        sourcemap: true,
+    },
 });
