1c5b5f4970a5890afae027192e3eb33038ee0952
yangxiong <978345079@qq.com>
2023-08-04 17:21:32 +0800

    feat: class Vector2

---
+++ src/classes/Vector2.ts
@@ -0,0 +1,84 @@
+class Vector2 {
+
+    x: number;
+    y: number;
+
+    constructor(x: number, y: number) {
+        this.x = x;
+        this.y = y;
+    }
+
+    get u() {
+        return this.x;
+    }
+
+    get v() {
+        return this.y;
+    }
+
+    get s() {
+        return this.x;
+    }
+
+    get t() {
+        return this.y;
+    }
+
+    clone() {
+        return new Vector2(this.x, this.y);
+    }
+
+    addFactor(v: number) {
+        this.x += v;
+        this.y += v;
+
+        return this;
+    }
+
+    add(v: Vector2) {
+        this.x += v.x;
+        this.y += v.y;
+
+        return this;
+    }
+
+    sub(v: Vector2) {
+        this.x -= v.x;
+        this.y -= v.y;
+
+        return this;
+    }
+
+    scale(v: number) {
+        this.x *= v;
+        this.y *= v;
+
+        return this;
+    }
+
+    multiply(v: Vector2) {
+        this.x *= v.x;
+        this.y *= v.y;
+
+        return this;
+    }
+
+    dot(v: Vector2) {
+        return this.x * v.x + this.y * v.y;
+    }
+
+    cross(v: Vector2) {
+        return this.x * v.y - this.y * v.x;
+    }
+
+    length() {
+        return Math.sqrt(this.x * this.x + this.y * this.y);
+    }
+
+    getNormal() {
+        return this.clone().scale(this.length());
+    }
+
+}
+
+export { Vector2 };
